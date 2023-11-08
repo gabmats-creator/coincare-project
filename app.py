abcd715d06a921889967c06c3ed48279d5bdbacb
Rafael-Yuki <rafael.tobaru@edu.unifil.br>
2023-11-07 21:15:54 -0300

    Adaptações Windows

--- app.py
+++ app.py
@@ -10,209 +10,208 @@ import functools
 import os
 import locale
 
-def create_app():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = os.environ.get(
-        "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFz"
-    )
-    app.config["MONGODB_URI"] = os.environ.get("MONGODB_URI")
-    app.db = MongoClient(app.config["MONGODB_URI"]).get_default_database()
 
-    def login_required(route):
-        @functools.wraps(route)
-        def route_wrapper(*args, **kwargs):
-            if session.get("email") is None:
-                return redirect(url_for(".login"))
-            
-            return route(*args, **kwargs)
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get(
+    "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFz"
+)
+app.config["MONGODB_URI"] = "mongodb+srv://gabmats:gabriel051@coincarecluster.rusm8wo.mongodb.net/CoinCare"
+app.db = MongoClient(app.config["MONGODB_URI"]).get_default_database()
+
+def login_required(route):
+    @functools.wraps(route)
+    def route_wrapper(*args, **kwargs):
+        if session.get("email") is None:
+            return redirect(url_for(".login"))
         
-        return route_wrapper
+        return route(*args, **kwargs)
     
-    def real_format(value):
-        locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
-        float_value = value
-        formatted_value = locale.currency(float_value, grouping=True, symbol=None)
+    return route_wrapper
 
-        return formatted_value
-    
-    def calc_free_value(user):
-        bills_data_value = current_app.db.bills.find({"_id": {"$in": user.bills}})
-        custo_contas = 0
-        for conta in bills_data_value:
-            custo_contas += float(conta["billValue"])
-        income = user.income
-        free_value = income - custo_contas
-        negative = True if free_value < 0 else None
-        if free_value < 0:
-            free_value *= (-1)
-        return real_format(free_value), negative
+def real_format(value):
+    locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
+    float_value = value
+    formatted_value = locale.currency(float_value, grouping=True, symbol=None)
 
-    @app.route("/")
-    def homepage():
-        current_theme = session.get("theme")
-        session["theme"] = current_theme
-        if session.get("user_id"):
-            del session["user_id"]
-        if session.get("email"):
-            del session["email"]
+    return formatted_value
 
-        return render_template("home.html")
+def calc_free_value(user):
+    bills_data_value = current_app.db.bills.find({"_id": {"$in": user.bills}})
+    custo_contas = 0
+    for conta in bills_data_value:
+        custo_contas += float(conta["billValue"])
+    income = user.income
+    free_value = income - custo_contas
+    negative = True if free_value < 0 else None
+    if free_value < 0:
+        free_value *= (-1)
+    return real_format(free_value), negative
 
-    @app.route("/home")
-    @login_required
-    def index():
-        user_data = current_app.db.users.find_one({"email": session["email"]})
-        user = User(**user_data)
-        bills_data = current_app.db.bills.find({"_id": {"$in": user.bills}}).sort("insertDate", -1).limit(3)
-        income_value = real_format(user.income)
-        free_value, negative = calc_free_value(user)
-        bill = [Bill(**bill) for bill in bills_data]
-
-        return render_template('index.html', title="Coincare - Início", bill_data=bill, user_name=user.name, user_income=income_value, free_value=free_value, negative=negative)
-    
-    @app.route("/contas", methods=["GET", "POST"])
-    @login_required
-    def bills_to_pay(confirm_delete=None, confirm_edit=None, bill=None):
-        user_data = current_app.db.users.find_one({"email": session["email"]})
-        user = User(**user_data)
-        if not bill:
-            bills_data = current_app.db.bills.find({"_id": {"$in": user.bills}})
-            bill = [Bill(**bill) for bill in bills_data]
-        
-        return render_template('bills_to_pay.html', title="Coincare - Contas a Pagar", bill_data=bill, confirm_delete=confirm_delete, confirm_edit=confirm_edit)
-    
-    @app.route("/add", methods=["GET", "POST"])
-    @login_required
-    def add_bill():
-        form = BillForm()
-
-        if form.validate_on_submit():
-            expire_date = str(form.expire_date.data)
-            expire_formatted = datetime.strptime(expire_date, '%Y-%m-%d').strftime("%d-%m-%Y")
-            insertion_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-            description = "" if not form.description.data else form.description.data
-            bill = Bill(
-                _id=uuid.uuid4().hex,
-                billName=form.bill_name.data,
-                billValue=form.bill_value.data,
-                expireDate=expire_formatted,
-                description=description,
-                insertDate=insertion_date
-            )
-            current_app.db.bills.insert_one(asdict(bill))
-            current_app.db.users.update_one(
-                {"_id": session["user_id"]}, {"$push": {"bills": bill._id}}
-            )
-
-            return redirect(url_for(".index"))
-
-        return render_template("new_bill.html", title="CoinCare - Adicionar Conta", form=form)
-    
-    @app.route("/conta/<string:_id>", methods=["GET", "POST"])
-    @login_required
-    def delete_bill(_id: str):
-        if request.method == "POST":
-            operacao = request.form.get('operacao')
-            if operacao == "excluir":
-                current_app.db.bills.delete_one({'_id': _id})
-
-            return redirect(url_for(".bills_to_pay"))
-
-        return bills_to_pay(confirm_delete=True)
-    
-    @app.route("/edit/<string:_id>", methods=["GET", "POST"])
-    @login_required
-    def edit_bill(_id: str):
-        operacao = request.form.get('operacao')
-        bills_data = current_app.db.bills.find({"_id": _id})
-        bill = [Bill(**bill) for bill in bills_data]
-        if request.method == "POST":
-            if operacao == "Confirmar":
-                if request.form.get("billName"):
-                    current_app.db.bills.update_one({"_id": _id},
-                    {"$set": {"billName": request.form.get("billName")}})
-                if request.form.get("billValue"):
-                    current_app.db.bills.update_one({"_id": _id},
-                    {"$set": {"billValue": request.form.get("billValue")}})
-                if request.form.get("expireDate"):
-                    expire_date = str(request.form.get("expireDate"))
-                    expire_formatted = datetime.strptime(expire_date, '%Y-%m-%d').strftime("%d-%m-%Y")
-                    current_app.db.bills.update_one({"_id": _id},
-                    {"$set": {"expireDate": expire_formatted}})
-                
-                current_app.db.bills.update_one({"_id": _id},
-                {"$set": {"description": request.form.get("description")}})
-
-            return redirect(url_for(".bills_to_pay"))
-        
-        return bills_to_pay(confirm_edit=True, bill=bill)
-
-    @app.get("/toggle-theme")
-    def toggle_theme():
-        current_theme = session.get("theme")
-        if current_theme == "dark":
-            session["theme"] = "light"
-        else:
-            session["theme"] = "dark"
-
-        return redirect(request.args.get("current_page"))
-    
-    @app.route("/registrar", methods=["GET", "POST"])
-    def register():
-        if session.get("email"):
-            return redirect(url_for(".index"))
-        form = RegisterForm()
-
-        if form.validate_on_submit():
-            if current_app.db.users.find_one({"email": form.email.data}):
-                flash("Este email já está em uso", category="danger")
-                return redirect(url_for(".register"))
-            else:
-                user = User(
-                    _id=uuid.uuid4().hex,
-                    name=form.nome.data,
-                    email=form.email.data,
-                    income=form.income.data,
-                    password=pbkdf2_sha256.hash(form.password.data),
-                )
-
-                current_app.db.users.insert_one(asdict(user))
-
-                flash("Usuário registrado com sucesso!!")
-                
-                return redirect(url_for(".login"))
-
-        return render_template("register.html", title="CoinCare - Registrar", form=form)
-    
-    @app.route("/logout")
-    def logout():
-        current_theme = session.get("theme")
-        session["theme"] = current_theme
+@app.route("/")
+def homepage():
+    current_theme = session.get("theme")
+    session["theme"] = current_theme
+    if session.get("user_id"):
         del session["user_id"]
+    if session.get("email"):
         del session["email"]
 
-        return redirect(url_for(".login"))
+    return render_template("home.html")
+
+@app.route("/home")
+@login_required
+def index():
+    user_data = current_app.db.users.find_one({"email": session["email"]})
+    user = User(**user_data)
+    bills_data = current_app.db.bills.find({"_id": {"$in": user.bills}}).sort("insertDate", -1).limit(3)
+    income_value = real_format(user.income)
+    free_value, negative = calc_free_value(user)
+    bill = [Bill(**bill) for bill in bills_data]
+
+    return render_template('index.html', title="Coincare - Início", bill_data=bill, user_name=user.name, user_income=income_value, free_value=free_value, negative=negative)
+
+@app.route("/contas", methods=["GET", "POST"])
+@login_required
+def bills_to_pay(confirm_delete=None, confirm_edit=None, bill=None):
+    user_data = current_app.db.users.find_one({"email": session["email"]})
+    user = User(**user_data)
+    if not bill:
+        bills_data = current_app.db.bills.find({"_id": {"$in": user.bills}})
+        bill = [Bill(**bill) for bill in bills_data]
     
-    @app.route("/login", methods=["GET", "POST"])
-    def login():
-        if session.get("email"):
-            return redirect(url_for(".index"))
-        
-        form = LoginForm()
+    return render_template('bills_to_pay.html', title="Coincare - Contas a Pagar", bill_data=bill, confirm_delete=confirm_delete, confirm_edit=confirm_edit)
 
-        if form.validate_on_submit():
-            user_data = current_app.db.users.find_one({"email": form.email.data})
-            if not user_data:
-                flash("Dados de login incorretos", category="danger")
-                return redirect(url_for(".login"))
-            user = User(**user_data)
+@app.route("/add", methods=["GET", "POST"])
+@login_required
+def add_bill():
+    form = BillForm()
 
-            if user and pbkdf2_sha256.verify(form.password.data, user.password):
-                session["user_id"] = user._id
-                session["email"] = user.email
+    if form.validate_on_submit():
+        expire_date = str(form.expire_date.data)
+        expire_formatted = datetime.strptime(expire_date, '%Y-%m-%d').strftime("%d-%m-%Y")
+        insertion_date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        description = "" if not form.description.data else form.description.data
+        bill = Bill(
+            _id=uuid.uuid4().hex,
+            billName=form.bill_name.data,
+            billValue=form.bill_value.data,
+            expireDate=expire_formatted,
+            description=description,
+            insertDate=insertion_date
+        )
+        current_app.db.bills.insert_one(asdict(bill))
+        current_app.db.users.update_one(
+            {"_id": session["user_id"]}, {"$push": {"bills": bill._id}}
+        )
 
-                return redirect(url_for(".index"))
+        return redirect(url_for(".index"))
+
+    return render_template("new_bill.html", title="CoinCare - Adicionar Conta", form=form)
+
+@app.route("/conta/<string:_id>", methods=["GET", "POST"])
+@login_required
+def delete_bill(_id: str):
+    if request.method == "POST":
+        operacao = request.form.get('operacao')
+        if operacao == "excluir":
+            current_app.db.bills.delete_one({'_id': _id})
+
+        return redirect(url_for(".bills_to_pay"))
+
+    return bills_to_pay(confirm_delete=True)
+
+@app.route("/edit/<string:_id>", methods=["GET", "POST"])
+@login_required
+def edit_bill(_id: str):
+    operacao = request.form.get('operacao')
+    bills_data = current_app.db.bills.find({"_id": _id})
+    bill = [Bill(**bill) for bill in bills_data]
+    if request.method == "POST":
+        if operacao == "Confirmar":
+            if request.form.get("billName"):
+                current_app.db.bills.update_one({"_id": _id},
+                {"$set": {"billName": request.form.get("billName")}})
+            if request.form.get("billValue"):
+                current_app.db.bills.update_one({"_id": _id},
+                {"$set": {"billValue": request.form.get("billValue")}})
+            if request.form.get("expireDate"):
+                expire_date = str(request.form.get("expireDate"))
+                expire_formatted = datetime.strptime(expire_date, '%Y-%m-%d').strftime("%d-%m-%Y")
+                current_app.db.bills.update_one({"_id": _id},
+                {"$set": {"expireDate": expire_formatted}})
+            
+            current_app.db.bills.update_one({"_id": _id},
+            {"$set": {"description": request.form.get("description")}})
+
+        return redirect(url_for(".bills_to_pay"))
+    
+    return bills_to_pay(confirm_edit=True, bill=bill)
+
+@app.get("/toggle-theme")
+def toggle_theme():
+    current_theme = session.get("theme")
+    if current_theme == "dark":
+        session["theme"] = "light"
+    else:
+        session["theme"] = "dark"
+
+    return redirect(request.args.get("current_page"))
+
+@app.route("/registrar", methods=["GET", "POST"])
+def register():
+    if session.get("email"):
+        return redirect(url_for(".index"))
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        if current_app.db.users.find_one({"email": form.email.data}):
+            flash("Este email já está em uso", category="danger")
+            return redirect(url_for(".register"))
+        else:
+            user = User(
+                _id=uuid.uuid4().hex,
+                name=form.nome.data,
+                email=form.email.data,
+                income=form.income.data,
+                password=pbkdf2_sha256.hash(form.password.data),
+            )
+
+            current_app.db.users.insert_one(asdict(user))
+
+            flash("Usuário registrado com sucesso!!")
+            
+            return redirect(url_for(".login"))
+
+    return render_template("register.html", title="CoinCare - Registrar", form=form)
+
+@app.route("/logout")
+def logout():
+    current_theme = session.get("theme")
+    session["theme"] = current_theme
+    del session["user_id"]
+    del session["email"]
+
+    return redirect(url_for(".login"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if session.get("email"):
+        return redirect(url_for(".index"))
+    
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user_data = current_app.db.users.find_one({"email": form.email.data})
+        if not user_data:
             flash("Dados de login incorretos", category="danger")
-        return render_template("login.html", title="CoinCare - Login", form=form)
+            return redirect(url_for(".login"))
+        user = User(**user_data)
+
+        if user and pbkdf2_sha256.verify(form.password.data, user.password):
+            session["user_id"] = user._id
+            session["email"] = user.email
+
+            return redirect(url_for(".index"))
+        flash("Dados de login incorretos", category="danger")
+    return render_template("login.html", title="CoinCare - Login", form=form)
     
-    return app
